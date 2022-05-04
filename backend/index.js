7496395b69007256aacc6a9dd269d8bfd8aa4279
dev <pdev0038@gmail.com>
2022-05-04 13:49:18 +0530

    make it better52

--- backend/index.js
+++ backend/index.js
@@ -34,10 +34,11 @@ if (process.env.NODE_ENV === "production") {
   console.log("here");
   const lik = path.join(__dirname, "../frontend");
   app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.send("frontend");
+  app.get(
+    "*",
+    (req, res) => res.send("frontend")
     //req.sendFile(path.resolve(lik, "build", "index.html"));
-  });
+  );
 }
 
 app.listen(port, () => {
